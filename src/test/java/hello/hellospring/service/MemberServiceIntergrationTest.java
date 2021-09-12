6772661482e0b23c8fd0585a1c9012673b717365
kimsangwoo66 <56182236+kimsangwoo66@users.noreply.github.com>
2021-09-12 21:58:56 +0900

    스프링부트기초1:스프링 통합 테스트

---
+++ src/test/java/hello/hellospring/service/MemberServiceIntergrationTest.java
@@ -0,0 +1,64 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional //이노테이션은 테스트가 끝나면 DB를 롤백 해주는 장점이있다. 테스트가 끝나면 DB반영이 안되게한다.
+class MemberServiceIntergrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+
+
+
+    @Test
+    void 회원가입() { //테스트 함수는 한글로 적어도 된다. 빌드될떄 실제 코드에 포함되지 않는다.
+        //given
+        Member member = new Member();
+        member.setName("spring");
+
+
+        //when
+        Long saveId = memberService.join(member);
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+
+        //멤버1 가입
+        memberService.join(member1);
+
+        //assertThrows: 테스트 라이브러리
+        //멤버2가 가입할떄 멤버1이랑 중복되면 테스트 성공
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+//
+        //then
+    }
+
+
+}
